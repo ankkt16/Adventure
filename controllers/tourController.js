73483d848de2a5e0b506e48184b8d4bf1496f0ec
Ankit <ankkt16@gmail.com>
2021-03-13 12:40:49 +0530

    Format code and add enviroment variables

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,86 +1,68 @@
 const fs = require('fs');
 
-
 const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, (err) => {
-      if (err) console.log(err);
-    })
-  );
-  
-  exports.getAllTours = (req, res) => {
-    res.status(200).json({
-      status: 'success',
-      result: tours.length,
-      data: {
-        tours,
-      },
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, (err) => {
+    if (err) console.log(err);
+  })
+);
+
+exports.checkId = (req, res, next, val) => {
+  if (tours.length <= val) {
+    return res.status(404).json({
+      status: 'failed',
+      message: 'No such tours exist',
     });
-  };
-  
-  exports.getTour = (req, res) => {
-    const id = req.params.id * 1;
-  
-    const tour = tours.find((el) => el.id === id);
-  
-    if (tour === undefined) {
-      res.status(404).json({
-        status: 'failed',
-        message: 'No such tours exist',
-      });
-    } else {
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour,
-        },
-      });
+  }
+  next();
+};
+
+exports.getAllTours = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    result: tours.length,
+    data: {
+      tours,
+    },
+  });
+};
+
+exports.getTour = (req, res) => {
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id == id);
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+};
+
+exports.createTour = (req, res) => {
+  const tourId = tours[tours.length - 1].id + 1;
+
+  const newTour = Object.assign({ id: tourId }, req.body);
+
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      // if (err) console.log(err);
+      res.status(201).json(newTour);
     }
-  };
-  
-  exports.createTour = (req, res) => {
-    const tourId = tours[tours.length - 1].id + 1;
-  
-    const newTour = Object.assign({ id: tourId }, req.body);
-  
-    tours.push(newTour);
-    fs.writeFile(
-      `${__dirname}/dev-data/data/tours-simple.json`,
-      JSON.stringify(tours),
-      (err) => {
-        // if (err) console.log(err);
-        res.status(201).json(newTour);
-      }
-    );
-  };
-  
-  exports. updateTour = (req, res) => {
-    const id = req.params.id * 1;
-  
-    if (tours.length <= id) {
-      res.status(404).json({
-        status: 'failed',
-        message: 'No such tours exist',
-      });
-    } else {
-      res.status(200).json({
-        status: 'success',
-        data: 'Updated',
-      });
-    }
-  };
-  
-  exports.deleteTour = (req, res) => {
-    const id = req.params.id * 1;
-  
-    if (tours.length <= id) {
-      res.status(404).json({
-        status: 'failed',
-        message: 'No such tours exist',
-      });
-    } else {
-      res.status(204).json({
-        status: 'success',
-        data: null,
-      });
-    }
-  };
+  );
+};
+
+exports.updateTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: 'Updated',
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+};
--- server.js
+++ server.js
@@ -1,6 +1,10 @@
-const app=require('./app');
+const dotenv = require('dotenv');
 
-const PORT = 3000;
+dotenv.config({ path: './config.env' });
+const app = require('./app');
+
+// console.log(process.env);
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
 });
