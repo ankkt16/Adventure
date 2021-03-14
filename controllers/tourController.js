f48e5bf9e75c6670bdd207d71433f9d9a336065c
Ankit <ankkt16@gmail.com>
2021-03-15 00:53:23 +0530

    Refactor code

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,57 +1,27 @@
-const fs = require('fs');
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`, (err) => {
-    if (err) console.log(err);
-  })
-);
-
-exports.checkId = (req, res, next, val) => {
-  if (tours.length <= val) {
-    return res.status(404).json({
-      status: 'failed',
-      message: 'No such tours exist',
-    });
-  }
-  next();
-};
+const Tour = require('../models/tourModels');
 
 exports.getAllTours = (req, res) => {
   res.status(200).json({
-    status: 'success',
-    result: tours.length,
-    data: {
-      tours,
-    },
+    // status: 'success',
+    // result: tours.length,
+    // data: {
+    //   tours,
+    // },
   });
 };
 
 exports.getTour = (req, res) => {
-  const id = req.params.id * 1;
-  const tour = tours.find((el) => el.id == id);
   res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
+    // status: 'success',
+    // data: {
+    //   tour,
+    // },
   });
 };
 
 exports.createTour = (req, res) => {
-  const tourId = tours[tours.length - 1].id + 1;
-
   // eslint-disable-next-line node/no-unsupported-features/es-syntax
-  const newTour = { id: tourId, ...req.body };
-
-  tours.push(newTour);
-  fs.writeFile(
-    `${__dirname}/dev-data/data/tours-simple.json`,
-    JSON.stringify(tours),
-    (err) => {
-      // if (err) console.log(err);
-      res.status(201).json(newTour);
-    }
-  );
+  // res.status(201).json(newTour);
 };
 
 exports.updateTour = (req, res) => {
