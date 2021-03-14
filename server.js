faab6c854b9903bcae330e59d9ba5cdbb6c11999
Ankit <ankkt16@gmail.com>
2021-03-14 22:50:32 +0530

    Connect to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,26 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+const DB = `mongodb+srv://Ankit:${process.env.DATABASE_PASSWORD}@cluster0.iidy3.mongodb.net/Adventure?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then((con) => {
+    console.log(con.connection);
+    console.log('Connection Successful');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // console.log(process.env);
 const { PORT } = process.env;
